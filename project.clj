6a8bfad3d7b36963f5a530e753f26fa8090692a7
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-03-17 21:30:58 +0100

    Version 0.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject opentype-clj "0.0.3-SNAPSHOT"
+(defproject opentype-clj "0.0.3"
   :description "Clojure (JVM) wrapper for opentype.js"
   :url "https://github.com/ivarref/opentype.clj"
   :license {:name "Eclipse Public License"

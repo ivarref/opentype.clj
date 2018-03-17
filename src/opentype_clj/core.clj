7404752c3d80c8292c56430ad05a148f9ebf393f
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-03-17 14:35:08 +0100

    Add simple interface

--- src/opentype_clj/core.clj
+++ src/opentype_clj/core.clj
@@ -1,7 +1,6 @@
 (ns opentype-clj.core
   (:require [opentype-clj.wrapper :as wrapper]
-            [opentype-clj.font-cache :as font-cache]
-            [clojure.string :as str]))
+            [opentype-clj.font-cache :as font-cache]))
 
 (defn font-name->font
   "Returns the font for font-name, or nil if the font cannot be found."
@@ -15,12 +14,19 @@
     (throw (ex-info "Could not find font-name" {:font-name font-name}))))
 
 (defn text
-  ([font-name s] (text font-name s 0 0 72 2))
-  ([font-name s x] (text font-name s x 0 72 2))
-  ([font-name s x y] (text font-name s x y 72 2))
-  ([font-name s x y font-size] (text font-name s x y font-size 2))
-  ([font-name s x y font-size decimals]
+  ([font-name txt] (text font-name txt 0 0 72 2))
+  ([font-name txt x] (text font-name txt x 0 72 2))
+  ([font-name txt x y] (text font-name txt x y 72 2))
+  ([font-name txt x y font-size] (text font-name txt x y font-size 2))
+  ([font-name txt x y font-size decimals]
    (let [font (font-name->font-or-throw font-name)
-         path (wrapper/get-path font s x y font-size)]
+         path (wrapper/get-path font txt x y font-size)]
      {:path-data    (wrapper/path->path-data path decimals)
       :bounding-box (wrapper/path->bounding-box path)})))
+
+;(defn text->path-data
+;  ([font-name txt] (text->path-data font-name txt 0 0 72 2))
+;  ([font-name txt x] (text->path-data font-name txt x 0 72 2))
+;  ([font-name txt x y] (text->path-data font-name txt x y 72 2))
+;  ([font-name txt x y font-size] (text->path-data font-name txt x y font-size 2))
+;  ([font-name txt x y font-size decimals] (:path-data (text font-name txt x y font-size decimals))))

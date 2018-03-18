4fd5c93e6103927eb92c40f9a1748bc4278f3cc3
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-03-18 21:12:47 +0100

    Bump rhino

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.474"]
-                 [org.mozilla/rhino "1.7.7.1"]
+                 [org.mozilla/rhino "1.7.9"]
                  [base64-clj "0.1.1"]
                  [org.apache.xmlgraphics/batik-rasterizer "1.9.1" :scope "test"]
                  [org.apache.xmlgraphics/batik-codec "1.9.1" :scope "test"]]

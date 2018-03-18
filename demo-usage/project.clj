91c80869d9994f69f74ffecf1202fbb47ffa42db
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-03-18 21:51:29 +0100

    Test code use latest dependency

--- demo-usage/project.clj
+++ demo-usage/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [opentype-clj "0.0.4"]])
+                 [opentype-clj "0.0.6"]])
